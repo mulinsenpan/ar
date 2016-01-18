0b69c3c100849e4c3f703d9bfa4f3edb8291122f
= <mulinsenpan@163.com>
2016-01-18 17:07:30 +0800

    segment 测试正常

--- segment.py
+++ segment.py
@@ -7,7 +7,7 @@
 import tools
 import copy
 
-SENSORLIST=[]
+SENSORLIST = []
 K = 3
 FRAME_SIZE = 10
 SIM_THETA = 0.1
@@ -23,9 +23,9 @@ def seg(sequences, frame_size=FRAME_SIZE, sim_theta=SIM_THETA):
     length = len(sequences)
     borders = []
     sim_list = []
-    for i in range(frame_size, length-frame_size + 1):
-        seq_left = sequences[i-frame_size:i]
-        seq_right = sequences[i:i+frame_size]
+    for i in range(frame_size, length - frame_size + 1):
+        seq_left = sequences[i - frame_size:i]
+        seq_right = sequences[i:i + frame_size]
         shared_sensors = set(seq_left).intersection(set(seq_right))
         if len(shared_sensors) == 0:
             borders.append(i)
@@ -33,12 +33,12 @@ def seg(sequences, frame_size=FRAME_SIZE, sim_theta=SIM_THETA):
         else:
             vector_left, matrix_left = convert2vector(seq_left)
             vector_right, matrix_right = convert2vector(seq_right)
-            sim = float(sim_cos(vector_left,vector_right))
+            sim = float(sim_cos(vector_left, vector_right))
             if sim <= sim_theta:
                 borders.append(i)
                 sim_list.append(sim)
-    new_borders = prunborders(borders,sim_list)
-    new_borders.append(length-1)
+    new_borders = prunborders(borders, sim_list)
+    new_borders.append(length - 1)
     return new_borders
 
 
@@ -48,8 +48,8 @@ def convert2vector(seq):
     for gap in range(K):
         index = 0
         sensor_matrix = [[0 for i in range(len(SENSORLIST))] for j in range(len(SENSORLIST))]
-        while index < len(seq)-gap-1:
-            sensor_matrix[seq[index]][seq[index+gap+1]] += 1
+        while index < len(seq) - gap - 1:
+            sensor_matrix[seq[index]][seq[index + gap + 1]] += 1
             index += 1
         matrix.append(sensor_matrix)
         for front in range(len(SENSORLIST)):
@@ -71,31 +71,32 @@ def distance_ed(vector1, vector2):
     sum_none_zero = 0
     while index < len(vector1):
         if vector1[index] != 0 and vector2[index] != 0:
-            sum_none_zero += (float(vector1[index]-vector2[index])**2)
+            sum_none_zero += (float(vector1[index] - vector2[index]) ** 2)
         if vector1[index] == 0 or vector2[index] == 0:
-            sum_zero += (float(vector1[index]-vector2[index])**2)
+            sum_zero += (float(vector1[index] - vector2[index]) ** 2)
         index += 1
-    ed =  (sum_none_zero ** 0.5) +  (sum_zero ** 0.5)
+    ed = (sum_none_zero ** 0.5) + (sum_zero ** 0.5)
     return ed
 
 
-def sim_cos(vector1,vector2):
+def sim_cos(vector1, vector2):
     """
     :param vector1: left
     :param vector2: right
     :return: 余弦相似度
     """
     numerator = sum((vector1[i] * vector2[i]) for i in range(len(vector1))) + 0.0
-    denominator = ((sum(value1 ** 2 for value1 in vector1))**0.5) * ((sum(value1 ** 2 for value1 in vector1))**0.5)
+    denominator = ((sum(value1 ** 2 for value1 in vector1)) ** 0.5) * ((sum(value1 ** 2 for value1 in vector1)) ** 0.5)
 
-    return numerator/float(denominator)
+    return numerator / float(denominator)
 
 
-def prunborders(borders,sim_list):
+def prunborders(borders, sim_list):
     new_borders = []
     index = 0
-    while index < len(borders)-2:
-        continue_borders = isContinue(borders,index)
+    while index < len(borders) - 2:
+        continue_borders = isContinue(borders, index)
+        print continue_borders
         if len(continue_borders) > 0:
             min_index = continue_borders[0]
             for value in continue_borders:
@@ -103,7 +104,7 @@ def prunborders(borders,sim_list):
                     min_index = value
             # min_index = sim_list.index(min(sim_list[continue_borders[0]:continue_borders[-1]]))
             new_borders.append(borders[min_index])
-            index = continue_borders[-1]+2
+            index = continue_borders[-1] + 2
         else:
             new_borders.append(borders[index])
             index += 1
@@ -112,7 +113,7 @@ def prunborders(borders,sim_list):
 
 def isContinue(borders, current):
     continue_index = []
-    for index in range(current,len(borders)):
+    for index in range(current, len(borders) - 1):
         later = index + 1
         try:
             if borders[later] - borders[index] < 10:
@@ -122,11 +123,18 @@ def isContinue(borders, current):
         except:
             print index, later
 
-
-# if __name__ == "__main__":
-#     borders =[10,11,13,16,19,35,49,80,112,113,114,158,190,200,250]
 #
-#     sim_list =[0.1,0.2,0.3,0.1,0.5,0.6,0.4,0.3,0.6,0.2,0.4,0.6,0.7,0.2,0.1]
-#     new_borders= prunborders(borders,sim_list)
-#     print new_borders
-
+# if __name__ == "__main__":
+#
+#     filename = "/home/chi/PycharmProjects/Seg_AR/data/example"
+#     SENSORLIST = tools.getSensorList(filename)
+#     seq_index = []
+#     with open(filename, 'r') as fr:
+#         for line in fr:
+#             row = line.split()
+#             seq_index.append(SENSORLIST.index(row[2]))
+#
+#
+#
+#     borders = seg(seq_index)
+#     print borders
